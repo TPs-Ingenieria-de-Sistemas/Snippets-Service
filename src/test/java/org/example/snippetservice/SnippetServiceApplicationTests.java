56b811ebb39fb3747b555af0abaa35eb5dca6a7a
santi <santiago.peres@ing.austral.edu.ar>
2024-06-02 15:20:27 -0300

    started project with spring

---
+++ src/test/java/org/example/snippetservice/SnippetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.snippetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnippetServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
