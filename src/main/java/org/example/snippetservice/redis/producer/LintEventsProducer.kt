28216d3bb2114e81f6d38c7cd2d340c54c8e5594
FerVassolo <63514959+Parrandos@users.noreply.github.com>
2024-07-19 13:01:05 -0300

    Redis now can recieve and send messages correctly.

---
+++ src/main/java/org/example/snippetservice/redis/producer/LintEventsProducer.kt
@@ -0,0 +1,28 @@
+package org.example.snippetservice.redis.producer
+
+import com.example.printscriptservice.redisEvents.LintRequestEvent
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.stereotype.Component
+import redisStreams.RedisStreamProducer
+
+@Component
+class LintEventsProducer
+    @Autowired
+    constructor(
+        @Value("\${redis.stream.linter-request-key}") streamKey: String,
+        redis: RedisTemplate<String, String>,
+    ) : RedisStreamProducer(streamKey, redis) {
+        fun publishEvent(
+            userID: String,
+            snippetID: String,
+            rules: String,
+            language: String,
+            version: String,
+        ) {
+            println("Publishing on stream: $streamKey")
+            val res = LintRequestEvent(userID, snippetID, rules, language, version)
+            emit(res)
+        }
+    }
