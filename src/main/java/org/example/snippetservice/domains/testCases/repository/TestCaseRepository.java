7c1af7e178beaca02f29cada7990fd899ebbfe55
FerVassolo <63514959+Parrandos@users.noreply.github.com>
2024-07-22 16:19:04 -0300

    Succesful conection between snippet and ps-service

--- src/main/java/org/example/snippetservice/domains/testCases/repository/TestCaseRepository.java
+++ src/main/java/org/example/snippetservice/domains/testCases/repository/TestCaseRepository.java
@@ -1,6 +1,7 @@
 package org.example.snippetservice.domains.testCases.repository;
 
 import java.util.List;
+
 import org.example.snippetservice.domains.testCases.model.TestCase;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,4 +12,5 @@ public interface TestCaseRepository extends JpaRepository<TestCase, Long> {
 	void deleteByOwnerIdAndFileNameAndTestCaseName(String ownerId, String fileName, String testCaseName);
 	void deleteByOwnerIdAndFileName(String ownerId, String fileName);
 	void deleteByOwnerId(String ownerId);
+    TestCase getById(Long id);
 }
--- src/main/java/org/example/snippetservice/domains/testCases/service/TestCaseService.java
+++ src/main/java/org/example/snippetservice/domains/testCases/service/TestCaseService.java
@@ -7,6 +7,10 @@ import org.example.snippetservice.domains.snippet.repository.SnippetRepository;
 import org.example.snippetservice.domains.testCases.dto.CreateTestCaseDTO;
 import org.example.snippetservice.domains.testCases.model.TestCase;
 import org.example.snippetservice.domains.testCases.repository.TestCaseRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -15,10 +19,15 @@ import TestCaseDTO.RunTestCaseDTO;
 
 @Service
 public class TestCaseService {
+
 	private final TestCaseRepository testCaseRepository;
 	private final RestTemplate restTemplate = new RestTemplate();
 	private final SnippetRepository snippetRepository;
-	private final String printscriptUrl = "http://printscript:8081/";
+
+    // TODO CAMBIAR A 8081
+	private final String printscriptUrl = "http://printscript-service:8084/";
+
+    Logger logger = LoggerFactory.getLogger(TestCaseService.class);
 
 	public TestCaseService(TestCaseRepository testCaseRepository, SnippetRepository snippetRepository) {
 		this.testCaseRepository = testCaseRepository;
@@ -33,12 +42,14 @@ public class TestCaseService {
 		// Missing validate user permits
 
 		TestCase finalTestCase;
+        logger.info("Creating test case for user: {}", createTestCaseDTO.getId());
 		if (createTestCaseDTO.getId() == null) {
 			String input = String.join(";", createTestCaseDTO.getInput());
 			String output = String.join(";", createTestCaseDTO.getOutput());
 			finalTestCase = this.testCaseRepository.save(new TestCase(ownerId, fileName,
 					createTestCaseDTO.getTestCaseName(), input, output, createTestCaseDTO.getEnv()));
 		} else {
+            logger.info("all {}", this.testCaseRepository.findAll());
 			TestCase testCase = this.testCaseRepository.findById(createTestCaseDTO.getId()).orElseThrow();
 			testCase.setTestCaseName(createTestCaseDTO.getTestCaseName());
 			testCase.setInput(String.join(";", createTestCaseDTO.getInput()));
@@ -68,9 +79,11 @@ public class TestCaseService {
 		RunTestCaseDTO runTestCaseDTO = new RunTestCaseDTO(snippet.getContent(), testCase.getInput(),
 				testCase.getOutput(), testCase.getEnv());
 		try {
-			return this.restTemplate.postForObject(printscriptUrl+"/test", runTestCaseDTO, Boolean.class);
+            logger.info("dto content {}, dto input {}, dto output {}, dto env {}", runTestCaseDTO.getContent(), runTestCaseDTO.getInput(), runTestCaseDTO.getOutput(), runTestCaseDTO.getEnv());
+
+			return this.restTemplate.postForObject(printscriptUrl + "test/execute", runTestCaseDTO, Boolean.class);
 		} catch (Exception e) {
-			throw new RuntimeException("Error executing tests");
+			throw new RuntimeException("Error executing tests", e);
 		}
 	}
 }
